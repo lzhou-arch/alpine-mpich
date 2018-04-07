918237bc9e2afcf5d451d62edf1f9dd9b6223176
llz <lzhou.arch@gmail.com>
2018-04-07 17:02:20 -0400

    add screen and change image name

--- cluster/dist_run.sh
+++ cluster/dist_run.sh
@@ -5,11 +5,12 @@ cp -r ../../lib/libmxnet.so mxnet
 export PS_VERBOSE=2
 
 python ../../tools/launch.py \
-   -n 1 \
+   -n 2 \
    -H ~/project/hosts \
    --sync-dst-dir /tmp/mxnet \
    python train_mnist.py \
      --network mlp \
-     --num-epochs 1 \
+     --num-epochs 2 \
+     --num-examples 1000 \
      --kv-store dist_sync
 
---
+++ cluster/local_run.sh
@@ -0,0 +1,6 @@
+cd ~/mxnet/example/image-classification
+
+python train_mnist.py \
+  --network mlp \
+  --num-epochs 2 \
+  --num-examples 1000
